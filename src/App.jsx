88ceb393099fd5b2fd37aefb44bcb218b6440280
Ise2005 <e9223@g.maizuru-ct.ac.jp>
2024-07-03 18:21:30 +0900

    フーガ

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function GethashID(hashIDName) {
+  if(hashIDName){
+    $('.tab li').find('a').each(function(){
+      var idName=$(this).attr('href');
+      if(idName==hashIDName){
+        var parentElm=$(this).parent();
+        $('.tab li').removeClass("active");
+        $(parentElm).addClass("active");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        $(".area").removeClass("is-active");
+        $(hashIDName).addClass("is-active");
+      }
+    });
+  }
 }
 
-export default App
+$('.tab a').on('click',function(){
+  var idName=$(this).attr('href');
+  GethashID(idName);
+  return false;
+});
+
+$(window).on('load', function(){
+  $('.tab li:first-of-type').addClass("active");
+  $('.area:first-of-type').addClass("is-active");
+  var hashName=location.hash;
+  GethashID(hashName);
+});
+
+document.querySelector('#top-button').addEventListener('click', (event) => {
+  const modelViewer = document.querySelector("model-viewer");
+  modelViewer.setAttribute('camera-orbit', "0deg 0deg 50%");
+});
+document.querySelector('#bottom-button').addEventListener('click', (event) => {
+  const modelViewer = document.querySelector("model-viewer");
+  modelViewer.setAttribute('camera-orbit', "180deg 0deg 20%");
+});
+document.querySelector('#right-button').addEventListener('click', (event) => {
+  const modelViewer = document.querySelector("model-viewer");
+  modelViewer.setAttribute('camera-orbit', "270deg 0deg 5%");
+});
+document.querySelector('#left-button').addEventListener('click', (event) => {
+  const modelViewer = document.querySelector("model-viewer");
+  modelViewer.setAttribute('camera-orbit', "90deg 0deg 50%");
+});
+
+export default App;
