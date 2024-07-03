ade26b121a84ea163726d4240121c18139da242d
Ise2005 <e9223@g.maizuru-ct.ac.jp>
2024-07-03 18:28:33 +0900

    カミノ

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Appコンポーネントをインポートする
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Router basename="/TanabeCastle/">
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+}
